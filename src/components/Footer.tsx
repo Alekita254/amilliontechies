f21a9b5a6d99a28250baf960befa28cb0be819be
Alekita254 <amurimi332@gmail.com>
2025-03-07 15:50:26 +0300

    I updated the footer for the project

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,160 +6,60 @@ export const Footer = () => {
       <hr className="w-11/12 mx-auto" />
 
       <section className="container py-20 grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-12 gap-y-8">
+        {/* Branding */}
         <div className="col-span-full xl:col-span-2">
-          <a
-            rel="noreferrer noopener"
-            href="/"
-            className="font-bold text-xl flex"
-          >
+          <a rel="noreferrer noopener" href="/" className="font-bold text-xl flex">
             <LogoIcon />
-            ShadcnUI/React
+            A Million Techies
           </a>
+          <p className="text-sm opacity-70 mt-2">
+            Empowering developers, designers, and tech enthusiasts worldwide.
+          </p>
         </div>
 
+        {/* Follow Us */}
         <div className="flex flex-col gap-2">
-          <h3 className="font-bold text-lg">Follow US</h3>
-          <div>
-            <a
-              rel="noreferrer noopener"
-              href="#"
-              className="opacity-60 hover:opacity-100"
-            >
-              Github
-            </a>
-          </div>
-
-          <div>
-            <a
-              rel="noreferrer noopener"
-              href="#"
-              className="opacity-60 hover:opacity-100"
-            >
-              Twitter
-            </a>
-          </div>
-
-          <div>
-            <a
-              rel="noreferrer noopener"
-              href="#"
-              className="opacity-60 hover:opacity-100"
-            >
-              Dribbble
-            </a>
-          </div>
+          <h3 className="font-bold text-lg">Follow Us</h3>
+          <a rel="noreferrer noopener" href="#" className="opacity-60 hover:opacity-100">Twitter</a>
+          <a rel="noreferrer noopener" href="#" className="opacity-60 hover:opacity-100">LinkedIn</a>
+          <a rel="noreferrer noopener" href="#" className="opacity-60 hover:opacity-100">GitHub</a>
+          <a rel="noreferrer noopener" href="#" className="opacity-60 hover:opacity-100">Discord</a>
         </div>
 
+        {/* Learning Resources */}
         <div className="flex flex-col gap-2">
-          <h3 className="font-bold text-lg">Platforms</h3>
-          <div>
-            <a
-              rel="noreferrer noopener"
-              href="#"
-              className="opacity-60 hover:opacity-100"
-            >
-              Web
-            </a>
-          </div>
-
-          <div>
-            <a
-              rel="noreferrer noopener"
-              href="#"
-              className="opacity-60 hover:opacity-100"
-            >
-              Mobile
-            </a>
-          </div>
-
-          <div>
-            <a
-              rel="noreferrer noopener"
-              href="#"
-              className="opacity-60 hover:opacity-100"
-            >
-              Desktop
-            </a>
-          </div>
+          <h3 className="font-bold text-lg">Resources</h3>
+          <a rel="noreferrer noopener" href="#" className="opacity-60 hover:opacity-100">Blogs</a>
+          <a rel="noreferrer noopener" href="#" className="opacity-60 hover:opacity-100">Courses</a>
+          <a rel="noreferrer noopener" href="#" className="opacity-60 hover:opacity-100">Workshops</a>
         </div>
 
+        {/* Open Source */}
         <div className="flex flex-col gap-2">
-          <h3 className="font-bold text-lg">About</h3>
-          <div>
-            <a
-              rel="noreferrer noopener"
-              href="#"
-              className="opacity-60 hover:opacity-100"
-            >
-              Features
-            </a>
-          </div>
-
-          <div>
-            <a
-              rel="noreferrer noopener"
-              href="#"
-              className="opacity-60 hover:opacity-100"
-            >
-              Pricing
-            </a>
-          </div>
-
-          <div>
-            <a
-              rel="noreferrer noopener"
-              href="#"
-              className="opacity-60 hover:opacity-100"
-            >
-              FAQ
-            </a>
-          </div>
+          <h3 className="font-bold text-lg">Open Source</h3>
+          <a rel="noreferrer noopener" href="#" className="opacity-60 hover:opacity-100">Contribute</a>
+          <a rel="noreferrer noopener" href="#" className="opacity-60 hover:opacity-100">Projects</a>
+          <a rel="noreferrer noopener" href="#" className="opacity-60 hover:opacity-100">Docs</a>
         </div>
 
+        {/* Events */}
         <div className="flex flex-col gap-2">
-          <h3 className="font-bold text-lg">Community</h3>
-          <div>
-            <a
-              rel="noreferrer noopener"
-              href="#"
-              className="opacity-60 hover:opacity-100"
-            >
-              Youtube
-            </a>
-          </div>
-
-          <div>
-            <a
-              rel="noreferrer noopener"
-              href="#"
-              className="opacity-60 hover:opacity-100"
-            >
-              Discord
-            </a>
-          </div>
-
-          <div>
-            <a
-              rel="noreferrer noopener"
-              href="#"
-              className="opacity-60 hover:opacity-100"
-            >
-              Twitch
-            </a>
-          </div>
+          <h3 className="font-bold text-lg">Events</h3>
+          <a rel="noreferrer noopener" href="#" className="opacity-60 hover:opacity-100">Meetups</a>
+          <a rel="noreferrer noopener" href="#" className="opacity-60 hover:opacity-100">Hackathons</a>
+          <a rel="noreferrer noopener" href="#" className="opacity-60 hover:opacity-100">Webinars</a>
         </div>
       </section>
 
+      {/* Copyright & CTA */}
       <section className="container pb-14 text-center">
-        <h3>
-          &copy; 2024 Landing page made by{" "}
-          <a
-            rel="noreferrer noopener"
-            target="_blank"
-            href="https://www.linkedin.com/in/leopoldo-miranda/"
-            className="text-primary transition-all border-primary hover:border-b-2"
-          >
-            Leo Miranda
+        <h3 className="text-sm opacity-80">
+          &copy; 2024 A Million Techies. Built for the community, by the community.
+        </h3>
+        <h3 className="mt-2">
+          Want to get involved?{" "}
+          <a rel="noreferrer noopener" href="#" className="text-primary transition-all border-primary hover:border-b-2">
+            Join Us
           </a>
         </h3>
       </section>
