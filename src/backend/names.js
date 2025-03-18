eef85ff80cccb1e8ec53503bc03b39301cf7f8f9
Alekita254 <amurimi332@gmail.com>
2025-03-18 18:16:32 +0300

    Now back to server

--- src/backend/names.js
+++ src/backend/names.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:5000/api";
+// export const API_URL = "http://localhost:5000/api";
 
-// export const API_URL = "https://milliapi.getotech.co.ke/api";
+export const API_URL = "https://milliapi.getotech.co.ke/api";
