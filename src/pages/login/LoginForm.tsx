bcb2557d8102e07e8259b59219fd1bcf9ebd26f8
Alekita254 <amurimi332@gmail.com>
2025-04-22 19:07:08 +0300

    New host

--- src/pages/login/LoginForm.tsx
+++ src/pages/login/LoginForm.tsx
@@ -18,7 +18,7 @@ export default function LoginForm() {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:5000/api/token/', {
+      const response = await fetch('https://millibackend.amilliontechies.com/api/token/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
