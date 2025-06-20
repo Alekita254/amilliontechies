62be9fc820c9c3fbc58e8379e180363a6da40eaf
Alekita254 <amurimi332@gmail.com>
2025-06-20 18:27:41 +0300

    Refactor CommunityPage component: remove commented-out social links and tags for cleaner code

--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -30,48 +30,7 @@ export const CommunityPage = () => {
     return <BlogLayout>Loading...</BlogLayout>;
   }
 
-  // const communityContent = {
-  //   socialLinks: [
-  //     {
-  //       id: 1,
-  //       name: "Facebook",
-  //       link: "https://facebook.com/community",
-  //       icon: "facebook",
-  //     },
-  //     {
-  //       id: 2,
-  //       name: "LinkedIn",
-  //       link: "https://linkedin.com/community",
-  //       icon: "linkedin",
-  //     },
-  //     {
-  //       id: 3,
-  //       name: "Twitter",
-  //       link: "https://twitter.com/community",
-  //       icon: "twitter",
-  //     },
-  //     {
-  //       id: 4,
-  //       name: "TikTok",
-  //       link: "https://tiktok.com/community",
-  //       icon: "tiktok",
-  //     },
-  //     {
-  //       id: 5,
-  //       name: "Discord",
-  //       link: "https://discord.com/community",
-  //       icon: "discord",
-  //     },
-  //     {
-  //       id: 6,
-  //       name: "WhatsApp",
-  //       link: "https://whatsapp.com/community",
-  //       icon: "whatsapp",
-  //     },
-      
-  //   ],
-  //   tags: ["Events", "Networking", "Workshops", "Social Media", "Community"],
-  // };
+
   const communityContent = {
     socialLinks: [
       { id: 1, name: "Facebook", link: "https://facebook.com/amilliontechies", icon: "facebook" },
