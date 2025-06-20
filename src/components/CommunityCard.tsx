2c358e1b7661dc021a57a3057eb5a0c1296e387f
Alekita254 <amurimi332@gmail.com>
2025-06-20 18:26:31 +0300

    Refactor CommunityCard component: replace div with Card component for improved structure and styling

--- src/components/CommunityCard.tsx
+++ src/components/CommunityCard.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
+import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card";
 
 interface CommunityCardProps {
   post: {
@@ -19,7 +20,7 @@ const CommunityCard = ({ post }: CommunityCardProps) => {
   };
 
   return (
-    <div className="bg-white shadow-lg rounded-xl overflow-hidden">
+    <Card>
       <img
         src={post.image}
         alt={post.title}
@@ -28,13 +29,19 @@ const CommunityCard = ({ post }: CommunityCardProps) => {
       />
 
       <div className="p-4">
-        <h3 className="text-lg font-bold">{post.title}</h3>
-        <p className="text-gray-600 mt-2">{post.description}</p>
+        <CardHeader className="space-y-1 flex md:flex-row justify-start items-start gap-4">
+          <CardTitle>{post.title}</CardTitle>
+        </CardHeader>
+        {/* <h3 className="text-lg font-bold">{post.title}</h3> */}
+        {/* <p className="text-gray-600 mt-2">{post.description}</p> */}
         {/* <Button className="mt-4" onClick={handleLearnMore}> */}
-          {/* Learn More */}
+        {/* Learn More */}
         {/* </Button> */}
+        <CardDescription className="text-md mt-2">
+          {post.description}
+        </CardDescription>
       </div>
-    </div>
+    </Card>
   );
 };
 
